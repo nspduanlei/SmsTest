e33c5f9d1a397448d96732907cb992e3ee3e7868
duanlei <455483293@qq.com>
2016-02-02 16:48:17 +0800

    Initial commit

---
+++ app/src/main/java/com/duanlei/smstest/MainActivity.java
@@ -0,0 +1,37 @@
+package com.duanlei.smstest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.HashMap;
+
+import cn.smssdk.EventHandler;
+import cn.smssdk.SMSSDK;
+import cn.smssdk.gui.RegisterPage;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SMSSDK.initSDK(this, "f53b8844032c", "46cb14cd0acb627b11f58c25f5dace76");
+
+        //打开注册页面
+        RegisterPage registerPage = new RegisterPage();
+        registerPage.setRegisterCallback(new EventHandler() {
+            public void afterEvent(int event, int result, Object data) {
+                // 解析注册结果
+                if (result == SMSSDK.RESULT_COMPLETE) {
+                    @SuppressWarnings("unchecked")
+                    HashMap<String, Object> phoneMap = (HashMap<String, Object>) data;
+                    String country = (String) phoneMap.get("country");
+                    String phone = (String) phoneMap.get("phone");
+                }
+            }
+        });
+
+        registerPage.show(this);
+    }
+}
---
+++ app/src/main/java/com/duanlei/smstest/Result.java
@@ -0,0 +1,17 @@
+package com.duanlei.smstest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Author: duanlei
+ * Date: 2016-02-02
+ */
+public class Result extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.result);
+    }
+}
